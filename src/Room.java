b9678194b89fd953ee493abf5e9e630f6bccb025
James <jshaklee2@ggc.edu>
2024-04-12 18:30:12 -0400

    made the room class and made a partial skeleton for it.

---
+++ src/Room.java
@@ -0,0 +1,13 @@
+public class Room{
+    int roomId;
+    String roomName, roomDesc, roomExits;
+    //Items[] roomInventory; //uncomment once the Items class is made
+
+    //unfinished constructor
+    Room(int roomId){
+
+    }
+    setRoomId(int id){
+
+    }
+}
