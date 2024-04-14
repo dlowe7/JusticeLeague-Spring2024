57fcd4271556432cadbb2f03aeebb6ce2688ad68
James <jshaklee2@ggc.edu>
2024-04-14 14:54:08 -0400

    Made the skeleton for the Map Class - Alex Shaklee

--- src/Map.java
+++ src/Map.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 Public class Map
 {
     //Field variables
@@ -8,8 +10,73 @@ Public class Map
     Monster[] allMonsters;
     Player allPlayerStats;
      */
+    Scanner fileReader;
 
     //Methods
+
+    //This method parses data for the rooms.
+    //Didn't include the parse exits method as that should
+    //be part of the readRooms method
+    public void readRooms(String roomFileName)
+    {
+
+    }
+
+    //this method parses data for the items
+    public void readItems(String itemFileName)
+    {
+
+    }
+
+    //this method parses data for the puzzle
+    public void readPuzzle(String puzzleFileName)
+    {
+
+    }
+
+    //this method parses data for the monsters
+    public void readMonster(String mosterFileName)
+    {
+
+    }
+
+    //All of the below methods will be uncommented out
+    //once the rest of the classes are made
+    /*
+    public List<Room> getAllRooms()
+    {
+
+    }
+
+    public List<Items> getAllItems()
+    {
+
+    }
+
+    public List<Puzzle> getAllPuzzles()
+    {
+
+    }
+
+    public List<Monster> getAllMonsters()
+    {
+
+    }
+
+    public List<Player> getAllPlayerStats()
+    {
+
+    }
+
+     */
+
+    //Honestly I'm not entirely sure what the purpose
+    //for this will be, but we will cross that bridge
+    //when we get there.
+    public void connectElements()
+    {
+
+    }
 
 
 }
