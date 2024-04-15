c3a704d0a42005fb77cfe1101d9e61801c4c2250
Nikki <nikkithao127@gmail.com>
2024-04-15 12:50:03 -0400

    Added commands to controller class and started building view skeleton -Nikki Thao

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,55 @@
 public class Controller {
-    
+
+    private void parseInput(String input) {
+        String[] inputParts = input.split(" ");
+        String command = inputParts[0];
+
+        switch (command) {
+
+            //Player FRs
+            case "move":
+                player.move(direction);
+                break;
+            case "inventory":
+                player.accessInventory();
+                break;
+            case "map":
+                player.accessMap();
+                break;
+            case "statistics":
+                player.accessStatistics();
+            case "save":
+            case "load":
+            case "exit":
+
+            //Item Frs
+            case "pickup":
+                player.pickup(itemName);
+                break;
+            case "drop":
+                player.drop(itemName);
+                break;
+            case "use":
+                player.use(itemName);
+                break;
+            case "consume":
+                player.consume(itemName);
+                break;
+            case "equip":
+                player.equip(itemName);
+                break;
+            case "unequip":
+                player.unequip();
+                break;
+
+            //Puzzle Frs
+            case
+
+            //Monster Frs
+            case
+
+            default:
+                view.displayMessage("Invalid command. Type 'help' for available commands.");
+        }
+    }
 }
--- src/View.java
+++ src/View.java
@@ -1,3 +1,19 @@
 public class View {
 
+    //Storyline
+    //This story follows a young warrior named Kael. Kael’s father, Finn, was once a famous knight,
+    // until he died at the hands of Slash, an elusive master swordsman. Consumed by rage and grief,
+    // Kael pledged an oath to avenge his father by defeating Slash. During his quest to find his
+    // father’s killer, Kael encounters many different enemies across the world, and acquires items
+    // that may aid him on his journey. The young warrior has found the dungeon where Slash resides,
+    // and he must now hunt him down.
+
+    public void displayWelcomeMessage() {
+
+    }
+
+    public void displayMessage(String message) {
+        System.out.println(message);
+        System.out.println();
+    }
 }
