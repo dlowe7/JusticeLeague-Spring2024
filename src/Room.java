8080d91a78241386e71a4edcc76ad75e182a0162
James <jshaklee2@ggc.edu>
2024-04-15 13:47:53 -0400

    Made the constructor for the room class to test the map class - Alex

--- src/Room.java
+++ src/Room.java
@@ -2,11 +2,15 @@ public class Room{
     //Field variables
     int roomId;
     String roomName, roomDesc, roomExits;
-    //Items[] roomInventory; //uncomment once the Items class is made
+    Items[] roomInventory;
 
     //unfinished constructor
-    Room(int roomId){
-
+    Room(int roomId, String roomName, String roomDesc, String roomExits, Items[] roomInventory){
+        this.roomId = roomId;
+        this.roomName = roomName;
+        this.roomDesc = roomDesc;
+        this.roomExits = roomExits;
+        this.roomInventory = roomInventory;
     }
 
     public void setRoomId(int id){
