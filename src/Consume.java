f6e64c6f3aa1cfd14d1a13c6fdc22d5c41b09735
James <jshaklee2@ggc.edu>
2024-04-19 20:54:13 -0400

    forgot about these changes as well - Alex

--- src/Consume.java
+++ src/Consume.java
@@ -4,7 +4,7 @@ public class Consume extends Items{
 	int healthIncrease;
 	int healthDecrease;
 	
-	public Consume(String name, String description, String itemID, int location, boolean isConsumable,
+	public Consume(String name, String description, String itemID, String location, boolean isConsumable,
 			boolean isEquipable, boolean isUsable, int healthIncrease, int healthDecrease) {
 		super(name, description, itemID, location, isConsumable, isEquipable, isUsable);
 		this.healthIncrease = healthIncrease;
--- src/Equipment.java
+++ src/Equipment.java
@@ -2,7 +2,7 @@
 public class Equipment extends Items{
 	int defenseIncrease, attackIncrease;
 
-	public Equipment(String name, String description, String itemID, int location, boolean isConsumable,
+	public Equipment(String name, String description, String itemID, String location, boolean isConsumable,
 			boolean isEquipable, boolean isUsable, int defenseIncrease, int attackIncrease) {
 		super(name, description, itemID, location, isConsumable, isEquipable, isUsable);
 		this.defenseIncrease = defenseIncrease;
