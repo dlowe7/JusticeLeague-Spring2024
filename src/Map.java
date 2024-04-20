2a8a376814949b6f379896f424e9bc5ce21ed716
James <jshaklee2@ggc.edu>
2024-04-19 21:15:57 -0400

    parsing items works - Alex

--- src/Map.java
+++ src/Map.java
@@ -5,143 +5,152 @@ import java.util.Scanner;
 
 public class Map
 {
-    //Field variables
-    ArrayList<Room> allRooms = new ArrayList<Room>();
-    ArrayList<Items> allItems = new ArrayList<Items>();
-    ArrayList<Puzzle> allPuzzles = new ArrayList<Puzzle>();
-    ArrayList<Monster> allMonsters = new ArrayList<Monster>();
-    Player allPlayerStats;
+	//Field variables
+	ArrayList<Room> allRooms = new ArrayList<Room>();
+	ArrayList<Items> allItems = new ArrayList<Items>();
+	ArrayList<Puzzle> allPuzzles = new ArrayList<Puzzle>();
+	ArrayList<Monster> allMonsters = new ArrayList<Monster>();
+	Player allPlayerStats;
 
-    Scanner fileReader;
+	Scanner fileReader;
 
-    //Methods
+	//Methods
 
-    //This method parses data for the rooms.
-    //Didn't include the parse exits method as that should
-    //be part of the readRooms method
-    public void readRooms(File roomFileName)
-    {
-    	Room r;
-    	try {
+	//This method parses data for the rooms.
+	//Didn't include the parse exits method as that should
+	//be part of the readRooms method
+	public void readRooms(File roomFileName)
+	{
+		Room r;
+		try {
 			fileReader = new Scanner(roomFileName);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	while(fileReader.hasNext()) {
-    	String roomId = fileReader.nextLine();
-    	String name = fileReader.nextLine();
-    	String description = fileReader.nextLine();
-    	String north = fileReader.nextLine();
-    	String east = fileReader.nextLine();
-    	String west = fileReader.nextLine();
-    	String south = fileReader.nextLine();
-    	r = new Room(roomId, name, description, north, east, west, south);
-    	allRooms.add(r);
-    	}
-    	
-    }
+		while(fileReader.hasNext()) {
+			String roomId = fileReader.nextLine();
+			String name = fileReader.nextLine();
+			String description = fileReader.nextLine();
+			String north = fileReader.nextLine();
+			String east = fileReader.nextLine();
+			String west = fileReader.nextLine();
+			String south = fileReader.nextLine();
+			r = new Room(roomId, name, description, north, east, west, south);
+			allRooms.add(r);
+		}
 
-    //this method parses data for the items
-    public void readItems(File itemFileName)
-    {
-    	try 
-    	{
+	}
+
+	//this method parses data for the items
+	public void readItems(File itemFileName)
+	{
+		try 
+		{
 			fileReader = new Scanner(itemFileName);
 		} catch (FileNotFoundException e) 
-    	{
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	while(fileReader.hasNext()) 
-    	{
-    		String name = fileReader.nextLine();
-    		String description = fileReader.nextLine();
-    		String itemID = fileReader.nextLine();
-    		String location = fileReader.nextLine(); 
+		while(fileReader.hasNext()) 
+		{
+			String name = fileReader.nextLine();
+			String description = fileReader.nextLine();
+			String itemID = fileReader.nextLine();
+			String location = fileReader.nextLine(); 
 			boolean isConsumable = fileReader.nextBoolean();
-    		boolean isEquipable = fileReader.nextBoolean();
-    		boolean isUsable = fileReader.nextBoolean();
-    		
-    		//just for initializing
-    		int defenseIncrease, attackIncrease;
-    		int healthIncrease, healthDecrease;
-    		String otherCondition;
-    		
-    		if(isEquipable) {
-    			defenseIncrease = fileReader.nextInt();
-    			attackIncrease = fileReader.nextInt();
-    		}
-    		else if(isEquipable) {
-    			healthIncrease = fileReader.nextInt();
-    			healthDecrease = fileReader.nextInt();
-    		}
-    		else if(isUsable){
-    			otherCondition = fileReader.nextLine();
-    		}
-    		
-    	}
-    	
-    	
-    }
+			boolean isEquipable = fileReader.nextBoolean();
+			boolean isUsable = fileReader.nextBoolean();
 
-    //this method parses data for the puzzle
-    public void readPuzzle(String puzzleFileName)
-    {
+			//just for initializing
+			int defenseIncrease, attackIncrease;
+			int healthIncrease, healthDecrease;
+			String otherCondition;
 
-    }
+			while(fileReader.hasNext()) 
+			{
+				if(isEquipable) 
+				{
+					defenseIncrease = fileReader.nextInt();
+					attackIncrease = fileReader.nextInt();
+					allItems.add(new Equipment(name, description, itemID, location, isConsumable, isEquipable, isUsable, defenseIncrease, attackIncrease));
+				}
+				else if(isEquipable) 
+				{
+					healthIncrease = fileReader.nextInt();
+					healthDecrease = fileReader.nextInt();
+					allItems.add(new Consume(name, description, itemID, location, isConsumable, isEquipable, isUsable, healthIncrease, healthDecrease));
+				}
+				else if(isUsable)
+				{
+					otherCondition = fileReader.nextLine();
+					allItems.add(new UniqueConditionItems(name, description, itemID, location, isConsumable, isEquipable, isUsable, otherCondition));
+				}
+			}
 
-    //this method parses data for the monsters
-    public void readMonster(File monsterFileName)
-    {
-    	try {
+		}
+
+
+	}
+
+	//this method parses data for the puzzle
+	public void readPuzzle(String puzzleFileName)
+	{
+
+	}
+
+	//this method parses data for the monsters
+	public void readMonster(File monsterFileName)
+	{
+		try {
 			fileReader = new Scanner(monsterFileName);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	while(fileReader.hasNext()) {
-    		String ID, name, description, abilities, location;
-    	    int exp, hp, attack;
-    	    ID = fileReader.nextLine();
-    	    name = fileReader.nextLine();
-    	    description = fileReader.nextLine();
-    	    abilities = fileReader.nextLine();
-    	    location = fileReader.nextLine();
-    	    exp = fileReader.nextInt();
-    	    hp = fileReader.nextInt();
-    	    attack = fileReader.nextInt();
-    	    //fileReader.next();//This line is to get rid of the buffer... or so I thought, but it might be needed once there is more data
-    	    
-    	    allMonsters.add(new Monster(ID, name, description, abilities, location, exp, hp, attack));
-    	}
-    }
-    
-    //This is to parse player information
-    public void readPlayerStats(File playerStats) {
-    	try {
+		while(fileReader.hasNext()) {
+			String ID, name, description, abilities, location;
+			int exp, hp, attack;
+			ID = fileReader.nextLine();
+			name = fileReader.nextLine();
+			description = fileReader.nextLine();
+			abilities = fileReader.nextLine();
+			location = fileReader.nextLine();
+			exp = fileReader.nextInt();
+			hp = fileReader.nextInt();
+			attack = fileReader.nextInt();
+			//fileReader.next();//This line is to get rid of the buffer... or so I thought, but it might be needed once there is more data
+
+			allMonsters.add(new Monster(ID, name, description, abilities, location, exp, hp, attack));
+		}
+	}
+
+	//This is to parse player information
+	public void readPlayerStats(File playerStats) {
+		try {
 			fileReader = new Scanner(playerStats);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	while(fileReader.hasNext()) {
-    		String startingRoomId;
-    		int maxHealth, currentHealth, level, baseDamage, exp;
-    	    //Items[] inventory; will add later
-    	    startingRoomId = fileReader.nextLine();
-    	    maxHealth = fileReader.nextInt();
-    	    currentHealth = fileReader.nextInt();
-    	    level = fileReader.nextInt();
-    	    baseDamage = fileReader.nextInt();
-    	    exp = fileReader.nextInt();
-    	    allPlayerStats = new Player(startingRoomId, maxHealth, currentHealth, level, baseDamage, exp);
-    	}
-    }
+		while(fileReader.hasNext()) {
+			String startingRoomId;
+			int maxHealth, currentHealth, level, baseDamage, exp;
+			//Items[] inventory; will add later
+			startingRoomId = fileReader.nextLine();
+			maxHealth = fileReader.nextInt();
+			currentHealth = fileReader.nextInt();
+			level = fileReader.nextInt();
+			baseDamage = fileReader.nextInt();
+			exp = fileReader.nextInt();
+			allPlayerStats = new Player(startingRoomId, maxHealth, currentHealth, level, baseDamage, exp);
+		}
+	}
 
-    //All of the below methods will be uncommented out
-    //once the rest of the classes are made
-    /*
+	//All of the below methods will be uncommented out
+	//once the rest of the classes are made
+	/*
     public List<Room> getAllRooms()
     {
 
@@ -167,15 +176,15 @@ public class Map
 
     }
 
-     */
+	 */
 
-    //Honestly I'm not entirely sure what the purpose
-    //for this will be, but we will cross that bridge
-    //when we get there.
-    public void connectElements()
-    {
+	//Honestly I'm not entirely sure what the purpose
+	//for this will be, but we will cross that bridge
+	//when we get there.
+	public void connectElements()
+	{
 
-    }
+	}
 
 
 }
