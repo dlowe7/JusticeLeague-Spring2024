4d8c69f55a3129b274381a51dfa1555eb98c41f2
Destiny Lowe <dlowe7@ggc.edu>
2024-04-20 22:31:44 -0400

    Added the Health Increase and Descrease methods within the Consume class by Destiny Lowe

--- src/Consume.java
+++ src/Consume.java
@@ -1,16 +1,27 @@
 
-public class Consume extends Items{
+public class Consume extends Items
+{
 
-	int healthIncrease;
-	int healthDecrease;
-	
+	private int healthIncrease;
+	private int healthDecrease;
+
 	public Consume(String name, String description, String itemID, String location, boolean isConsumable,
-			boolean isEquipable, boolean isUsable, int healthIncrease, int healthDecrease) {
-		super(name, description, itemID, location, isConsumable, isEquipable, isUsable);
-		this.healthIncrease = healthIncrease;
-		this.healthDecrease = healthDecrease;
-	}
+			boolean isEquipable, boolean isUsable, int healthIncrease, int healthDecrease) 
+		{
+			super(name, description, itemID, location, isConsumable, isEquipable, isUsable);
+			this.healthIncrease = healthIncrease;
+			this.healthDecrease = healthDecrease;
+		}
 
-	
+	public int getHealthIncrease() 
+		{
+			return healthIncrease;
+		}
+
+	public int getHealthDecrease() 
+		{
+			return healthDecrease;
+		}
+
 
 }
