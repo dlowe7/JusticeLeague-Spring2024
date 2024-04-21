c377b5dd553267e0efe4e184ea0e15db93014bc1
Destiny Lowe <dlowe7@ggc.edu>
2024-04-20 22:40:20 -0400

    Added a few getters and a method to calculate
    rewards in the Monster class by Destiny Lowe

--- src/Monster.java
+++ src/Monster.java
@@ -33,5 +33,28 @@ public class Monster
     	return abilities;
     }
     
+    public int calculateRewards() {
+        int rewards = 0;
+
+        // Example: Calculate rewards based on monster level
+        int monsterLevel = this.exp / 100; // Assuming 100 EXP per level
+        rewards += monsterLevel * 5; // Example: Each level of the monster rewards 5 points
+
+        // Example: Calculate rewards based on monster type
+        if (this.name.equals("Dragon")) {
+            rewards += 50; // Example: Defeating a dragon grants additional rewards
+        } else if (this.name.equals("Orc")) {
+            rewards += 20; // Example: Defeating an orc grants moderate rewards
+        } else {
+            rewards += 10; // Default rewards for other types of monsters
+        }
+
+        // Example: Calculate rewards based on monster difficulty
+        // Assuming difficulty is based on HP and attack
+        int monsterDifficulty = (this.hp + this.attack) / 10; // Example: Higher difficulty monsters grant more rewards
+        rewards += monsterDifficulty * 10;
+
+        return rewards;
+    }
     
 }
