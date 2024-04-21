6505389f011a5dc6bf8f6d08031e6f41970ac222
Destiny Lowe <dlowe7@ggc.edu>
2024-04-20 22:24:27 -0400

    Added the Defense and Attack Increase for the Equipment by Destiny Lowe

--- src/Equipment.java
+++ src/Equipment.java
@@ -1,14 +1,26 @@
 
-public class Equipment extends Items{
-	int defenseIncrease, attackIncrease;
+public class Equipment extends Items
+{
+	private int defenseIncrease;
+	private int attackIncrease;
 
 	public Equipment(String name, String description, String itemID, String location, boolean isConsumable,
-			boolean isEquipable, boolean isUsable, int defenseIncrease, int attackIncrease) {
+			boolean isEquipable, boolean isUsable, int defenseIncrease, int attackIncrease) 
+		{
 		super(name, description, itemID, location, isConsumable, isEquipable, isUsable);
 		this.defenseIncrease = defenseIncrease;
 		this.attackIncrease = attackIncrease;
 	}
 	
+	public int getDefenseIncrease() 
+		{
+        return defenseIncrease;
+    }
+	
+	public int getAttackIncrease() 
+		{
+        return attackIncrease;
+    }
 	
 
 }
