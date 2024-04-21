4ee5128cd3d3db28382de1696687ae7871903cb2
Destiny Lowe <dlowe7@ggc.edu>
2024-04-21 11:14:04 -0400

    Fixed the Main class to be integrated with the
    Controller. I passed an instance of the Map class to the Controller
    constructor.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,23 @@
 import java.io.File;
 
-//import java.lang;
-
-public class Main
+public class Main 
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world");
-        Map game = new Map();
-        game.readRooms(new File("Rooms.txt"));
-        game.readMonster(new File("Monsters.txt"));
-        game.readPlayerStats(new File("Player.txt"));
-        game.readItems(new File("Items.txt"));
-        game.readPuzzle(new File("Puzzles.txt"));
-        
-        //game.readItems(new File("Items.txt"));
-        //game.readPuzzles(new File("Puzzles.txt"));
-    }
+	public static void main(String[] args) 
+		{
+			// Create an instance of Map
+			Map game = new Map();
+
+			// Read data from files and populate game objects
+			game.readRooms(new File("Rooms.txt"));
+			game.readItems(new File("Items.txt"));
+			game.readPuzzle(new File("Puzzles.txt"));
+			game.readMonster(new File("Monsters.txt"));
+			game.readPlayerStats(new File("Player.txt"));
+
+			// Create an instance of Controller and pass the game instance to its constructor
+			Controller controller = new Controller(game);
+
+			// Start the game
+			controller.startGame();
+		}
 }
